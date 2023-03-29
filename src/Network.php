e87ea6772c901883719dae5dd694a27e8f48bf67
InfinyHost LLC <hi@infinyhost.com>
2023-03-29 08:18:02 +0300

    Podman pod and network features additions

---
+++ src/Network.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace InfinyHost\CpUtils;
+
+use Symfony\Component\Process\Process;
+
+class Network
+{
+    public static function userContainerIP(int $uid, string $network="Customers", int $subtract=1000)
+    {
+        $range = self::userContainerGateway($network);
+
+        // Get the IP address of the user
+        $range = ip2long($range);
+        // +1 because we want to start from the second IP address, first being the gateway
+        $ip = $range - $subtract + $uid + 1;
+        $result = long2ip($ip);
+        if ($result === false) {
+            throw new \RuntimeException("Failed getting IP address for user " . $uid . " in network " . $network);
+        }
+        return $result;
+    }
+
+    public static function userContainerGateway(string $network)
+    {
+        $range = '';
+        $process = new Process([Podman::$podman, 'network', 'inspect', $network]);
+        $process->run();
+        if ($process->isSuccessful()) {
+            $network = json_decode($process->getOutput(), true);
+            if ($network === false || !is_array($network) || count($network) == 0) {
+                throw new \RuntimeException("Failed getting network info for " . $network . " : " . $process->getErrorOutput());
+            }
+            $net = $network[0];
+            foreach ($net['plugins'] as $plugin) {
+                if (isset($plugin['ipam'])) {
+                    if (isset($plugin['ipam']['ranges'][0]) && isset($plugin['ipam']['ranges'][0][0]) && isset($plugin['ipam']['ranges'][0][0]['gateway'])) {
+                        $range = $plugin['ipam']['ranges'][0][0]['gateway'];
+                        break;
+                    }
+                    // Not the network we are looking for.. Move on.
+                }
+            }
+        } else {
+            throw new \RuntimeException("Failed getting network info for " . $network . " : " . $process->getErrorOutput());
+        }
+        return $range;
+    }
+
+}
--- src/Podman.php
+++ src/Podman.php
@@ -50,9 +50,119 @@ class Podman
             $pod->__fromJson($process->getOutput());
             return $pod;
         } else {
-            throw new PodNotFoundException('Container not found');
+            throw new PodNotFoundException('Pod not found');
         }
     }
+
+    /**
+     * @throws PodNotFoundException
+     */
+    public static function podCreate(string $name, array $args = []): Pod
+    {
+        $process = new Process([self::$podman, 'pod', 'create','--infra-name', "infra-" . $name, '--name', $name, ...$args]);
+        $process->mustRun();
+        return self::podInspect($name);
+    }
+
+    /**
+     * Check if a pod is running
+     * @param string $pod
+     * @return bool
+     */
+    public static function podIsRunning(string $pod): bool
+    {
+        $process = new Process([self::$podman, 'pod', 'top', $pod]);
+        $process->run();
+        return $process->isSuccessful();
+    }
+
+    /**
+     * Start a pod
+     * @param string $pod
+     * @return void
+     */
+    public static function podStart(string $pod): void
+    {
+        $process = new Process([self::$podman, 'pod', 'start', $pod]);
+        $process->mustRun();
+    }
+
+    /**
+     * Stop a pod
+     * @param string $pod
+     * @return void
+     */
+    public static function podStop(string $pod): void
+    {
+        $process = new Process([self::$podman, 'pod', 'stop', $pod]);
+        $process->mustRun();
+    }
+
+    /**
+     * Kill a pod
+     * @param string $pod
+     * @return void
+     */
+    public static function podKill(string $pod): void
+    {
+        $process = new Process([self::$podman, 'pod', 'kill', $pod]);
+        $process->mustRun();
+    }
+
+    /**
+     * Restart a pod
+     * @param string $pod
+     * @return void
+     */
+    public static function podRestart(string $pod): void
+    {
+        $process = new Process([self::$podman, 'pod', 'restart', $pod]);
+        $process->mustRun();
+    }
+
+    /**
+     * Pause a pod
+     * @param string $pod
+     * @return void
+     */
+    public static function podPause(string $pod): void
+    {
+        $process = new Process([self::$podman, 'pod', 'pause', $pod]);
+        $process->mustRun();
+    }
+
+    /**
+     * Unpause a pod
+     * @param string $pod
+     * @return void
+     */
+    public static function podUnpause(string $pod): void
+    {
+        $process = new Process([self::$podman, 'pod', 'unpause', $pod]);
+        $process->mustRun();
+    }
+
+    /**
+     * Remove a pod
+     * @param string $pod
+     * @return void
+     */
+    public static function podRm(string $pod): void
+    {
+        $process = new Process([self::$podman, 'pod', 'rm', $pod]);
+        $process->mustRun();
+    }
+
+    /**
+     * Prune all pods
+     * @return void
+     */
+    public static function podPrune(): void
+    {
+        $process = new Process([self::$podman, 'pod', 'prune']);
+        $process->mustRun();
+    }
+
 
 
 }
